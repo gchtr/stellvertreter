6344106724b6df2811d0a325bc9c74ebc87148e5
Lukas Gächter <lukas.gaechter@zhdk.ch>
2015-01-13 00:30:01 +0100

    remove clutter, improve debug output

--- web/js/connect.js
+++ web/js/connect.js
@@ -5,13 +5,17 @@ var uri = URI('ws://7dc71b64220f9034:60277088aaf9b5e71e1a5dfbeef76156@connect.sh
 var client = new Paho.MQTT.Client('ws://' + uri.host() + '/', 'webPlayer');
 
 client.onConnectionLost = function(res) {
-  if(res.errorCode !== 0) {
+  if (res.errorCode !== 0) {
     console.log('connection has been lost');
   }
+  console.log(res);
 };
 
 client.onMessageArrived = function(message) {
-  console.log('new message: ', message.payloadString);
+  var msg = message.payloadString;
+  //console.log('new message: ', message.payloadString);
+
+  printData(msg);
 };
 
 client.connect({
@@ -19,12 +23,9 @@ client.connect({
   password: uri.password(),
   onSuccess: function() {
     client.subscribe("/input");
-    message = new Paho.MQTT.Message('Hello world!');
-    message.destinationName = "/input";
-    client.send(message);
   },
-  onFailure: function(lala) {
-    console.log(lala);
+  onFailure: function(failure) {
+    console.log(failure);
     console.log('not connected');
   }
 });
