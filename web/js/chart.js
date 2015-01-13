55b531f11ddcb234525575b31300773835ea5fe5
Lukas Gächter <lukas.gaechter@zhdk.ch>
2015-01-14 00:39:40 +0100

    add Rickshaw for live view

--- web/js/chart.js
+++ web/js/chart.js
@@ -1,54 +1,93 @@
-var n = 40;
-var data = d3.range(n).map(function() {
-  return 0;
+// set up our data series with 150 random data points
+
+var empties = 50;
+var initValue = 0.1;
+var timeCount = empties + 1;
+var seriesData = [ [], [], [], [] ];
+var isRunning = true;
+
+seriesData.forEach(function(series, index) {
+  for (var i = 0; i < empties; i++) {
+    series.push({x: i, y: initValue});
+  }
 });
 
-console.log(data);
+var graph = new Rickshaw.Graph( {
+  element: document.getElementById("chart"),
+  width: 1200,
+  height: 500,
+  renderer: 'area',
+  stroke: true,
+  preserve: true,
+  series: [
+    {
+      color: '#e6ad47',
+      data: seriesData[0],
+      name: 'Links vorne'
+    }, {
+      color: '#ff5755',
+      data: seriesData[1],
+      name: 'Links hinten'
+    }, {
+      color: '#64d182',
+      data: seriesData[2],
+      name: 'Rechts vorne'
+    }, {
+      color: '#8e36d9',
+      data: seriesData[3],
+      name: 'Rechts hinten'
+    }
+  ]
+} );
 
-var margin = {top: 20, right: 20, bottom: 20, left: 40},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+graph.render();
 
-var x = d3.scale.linear()
-    .domain([1, n - 2])
-    .range([0, width]);
+var legend = new Rickshaw.Graph.Legend({
+    graph: graph,
+    element: document.querySelector('#legend')
+});
 
-var y = d3.scale.linear()
-    .domain([0, 1])
-    .range([height, 0]);
+var shelving = new Rickshaw.Graph.Behavior.Series.Toggle({
+    graph: graph,
+    legend: legend
+});
 
-var line = d3.svg.line()
-    .interpolate("basis")
-    .x(function(d, i) { return x(i); })
-    .y(function(d, i) { return y(d); });
+var highlighter = new Rickshaw.Graph.Behavior.Series.Highlight({
+    graph: graph,
+    legend: legend
+});
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var ticksTreatment = 'glow';
 
-svg.append("defs").append("clipPath")
-    .attr("id", "clip")
-  .append("rect")
-    .attr("width", width)
-    .attr("height", height);
+var xAxis = new Rickshaw.Graph.Axis.Time( {
+  graph: graph,
+  ticksTreatment: ticksTreatment,
+  timeFixture: new Rickshaw.Fixtures.Time.Local()
+} );
 
-svg.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + y(0) + ")")
-    .call(d3.svg.axis().scale(x).orient("bottom"));
+xAxis.render();
 
-svg.append("g")
-    .attr("class", "y axis")
-    .call(d3.svg.axis().scale(y).orient("left"));
+var yAxis = new Rickshaw.Graph.Axis.Y( {
+  graph: graph,
+  tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
+  ticksTreatment: ticksTreatment,
+} );
 
-var path = svg.append("g")
-    .attr("clip-path", "url(#clip)")
-  .append("path")
-    .datum(data)
-    .attr("class", "line")
-    .attr("d", line);
+yAxis.render();
+
+function pushValues(numbers) {
+  var rangeMax = 1023;
+
+  seriesData.forEach(function(series, index) {
+    var y = numbers[index]/rangeMax;
+    var x = timeCount;
+    series.shift();
+    series.push({x: x, y: y});
+  });
+
+  timeCount++;
+  graph.update();
+}
 
 function printData(msg) {
   /**
@@ -67,32 +106,49 @@ function printData(msg) {
     return parseInt(el, 10);
   });
 
-  pushValues(numbers);
+  if (isRunning) {
+    pushValues(numbers);
+  }
+
 }
 
-function pushValues(numbers) {
-  var value = numbers[0]/1023;
+var Controls = {
 
-  console.log(value);
-  // push a new data point onto the back
-  data.push(value);
+  el: {
+    $play: $('#play'),
+    $pause: $('#pause'),
+    $reset: $('#reset')
+  },
 
-  // pop the old data point off the front
-  data.shift();
+  init: function() {
+    this.bindEvents();
+  },
+
+  bindEvents: function() {
+    this.el.$play.on('click', this.play);
+    this.el.$pause.on('click', this.pause);
+    this.el.$reset.on('click', this.reset);
+  },
+
+  play: function() {
+    isRunning = true;
+  },
+
+  pause: function() {
+    isRunning = false;
+  },
+
+  reset: function() {
+    seriesData.forEach(function(series) {
+      for (var i = 0; i < empties; i++) {
+        series[i] = {x: i, y: initValue};
+      }
+    });
+    timeCount = empties + 1;
+  }
+
 }
 
-// first tick
-tick();
-
-function tick() {
-
-  // redraw the line, and slide it to the left
-  path
-      .attr("d", line)
-      .attr("transform", null)
-    .transition()
-      .duration(1000)
-      .ease("linear")
-      .attr("transform", "translate(" + x(0) + ",0)")
-      .each("end", tick);
-}
+$(document).ready(function() {
+  Controls.init();
+});
